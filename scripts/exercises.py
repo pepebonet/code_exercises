ffcee38e6e5d0e1762d31296d321b3dbab599fdb
josebonet <jose.bonet@irbbarcelona.org>
2022-01-17 15:30:15 +0100

    initialize first exercise

---
+++ scripts/exercises.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def reverseWords(s):
+    output = ""
+    for el in s.split(' '):
+        import pdb;pdb.set_trace()
+        print(el)
+    return output
+
+
+
+if __name__ == '__main__':
+    reverseWords("Hello, John")
