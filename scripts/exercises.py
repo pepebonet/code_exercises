ed5a28cb6ab22eb8fe85884bb5fdeab24889eabc
josebonet <jose.bonet@irbbarcelona.org>
2022-01-18 16:46:46 +0100

    quickSort

--- scripts/exercises.py
+++ scripts/exercises.py
@@ -36,10 +36,44 @@ def printthepattern(pattern):
 
     return [item for sublist in final for item in sublist]
 
+
+def partition(arr, low, high):
+    i = (low-1) 
+    pivot = arr[high]   
+
+    for j in range(low, high):
+
+        if arr[j] <= pivot:
+
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+            
+    arr[i+1], arr[high] = arr[high], arr[i+1]
     
+    return (i+1)
+    
+
+def quickSort(arr, low, high):
+
+    if len(arr) == 1:
+        return arr
+
+    if low < high:
+
+        pi = partition(arr, low, high)
+
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+ 
 
 
 if __name__ == '__main__':
     print(reverseWords("Hello, John"))
 
     print(printthepattern(3))
+
+    arr = [10, 7, 5, 3, 2, 4]
+    n = len(arr)
+    print(quickSort(arr, 0, n-1))
+
+    
