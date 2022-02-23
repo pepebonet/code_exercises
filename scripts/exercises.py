ddefc193be1a837a5608a51de5283d0a6d22bbb7
josebonet <jose.bonet@irbbarcelona.org>
2022-02-23 10:09:06 +0100

    trap water exercise

--- scripts/exercises.py
+++ scripts/exercises.py
@@ -327,6 +327,38 @@ def minDist(arr, n, x, y):
         return -1
 
 
+def trap_water(arr, n):
+    # left[i] contains height of tallest bar to the 
+    # left of ith bar including itself. 
+    left = [0]*n 
+    
+    # right[i] contains height of tallest bar to 
+    # the right of ith bar including itself.
+    right = [0]*n 
+    
+    water = 0
+    
+    # Storing values of tallest bar from first index till ith index.
+    left[0] = arr[0] 
+    for i in range( 1, n): 
+        left[i] = max(left[i-1], arr[i]) 
+    
+    # Storing values of tallest bar from last index till ith index.
+    right[n-1] = arr[n-1] 
+    for i in range(n-2, -1, -1): 
+        right[i] = max(right[i+1], arr[i]); 
+    
+    # Storing the result by choosing the minimum of heights of tallest bar to
+    # the right and left of the bar at current index and also subtracting the
+    # value of current index to get water accumulated at current index.
+    for i in range(0, n): 
+        water += min(left[i],right[i]) - arr[i] 
+    
+    # returning the result.
+    return water 
+    
+
+
 if __name__ == '__main__':
     # print(reverseWords("Hello, John"))
 
@@ -359,4 +391,5 @@ if __name__ == '__main__':
 
     # print(NthRoot(6, 4096))
 
-    minDist([1,2,3,2], 4, 1, 2)
+    # minDist([1,2,3,2], 4, 1, 2)
+    trap_water([3,0,0,2,0,4], 6)
